adb766df8abc85b812c07672089f13637663acec
Daniel Felizardo <daniel.david772@gmail.com>
2020-01-11 20:04:43 -0300

    feat: create configuration for application with react

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+import '~/config/ReactotronConfig';
+
+import Routes from '~/routes';
+import history from '~/services/history';
+import GlobalStyle from '~/styles/global';
+
+function App() {
+  return (
+    <Router history={history}>
+      <Routes />
+      <GlobalStyle />
+      <ToastContainer autoClose={3000} />
+    </Router>
+  );
 }
+
+export default App;
