b03a070218c734f93c265b32e7fb97a4fa23c27e
Daniel Felizardo <daniel.david772@gmail.com>
2022-03-26 15:29:06 -0300

    Fix adiciona seed user admin #2

--- packages/server/src/database/seeds/admin.ts
+++ packages/server/src/database/seeds/admin.ts
@@ -1,6 +1,16 @@
-export const admin = {
-  admin: true,
-  email: 'daniel.david772@gmail.com',
-  nick: 'admin',
-  name: 'Daniel Felizardo'
+import { PrismaClient } from '@prisma/client'
+
+export async function seederAdmin(prismaClient: PrismaClient) {
+  const existAdmin = await prismaClient.user.findFirst({
+    where: { admin: true, email: 'daniel.david772@gmail.com' }
+  })
+
+  if (!existAdmin) {
+    return {
+      admin: true,
+      email: 'daniel.david772@gmail.com',
+      nick: 'admin',
+      name: 'Daniel Felizardo'
+    }
+  }
 }
