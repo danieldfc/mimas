e7ce3d734b67ad53270648f838ef36553bbb927f
Daniel Felizardo <daniel.david772@gmail.com>
2022-03-27 02:33:15 -0300

    Ajusta testes

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,11 @@
-export default {
+module.exports = {
   clearMocks: true,
-  coverageProvider: 'v8',
   preset: 'ts-jest',
   projects: ['<rootDir>/packages/**/jest.config.ts'],
   testEnvironment: 'node',
-  testMatch: ['*.spec.ts', '*.spec.tsx']
+  testMatch: ['*.spec.ts', '*.spec.tsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '\\.js$': 'babel-jest'
+  }
 }
--- packages/server/jest.config.ts
+++ packages/server/jest.config.ts
@@ -10,6 +10,10 @@ export default {
   collectCoverageFrom: ['<rootDir>/src/modules/**/services/*.ts'],
   coverageDirectory: 'coverage',
   testMatch: ['**/*.spec.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '\\.js$': 'babel-jest'
+  },
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>'
   })
