d97aa86a6e2d3dce1477c4247dd92c2ea32781b7
danielfelizardo <daniel.david772@gmail.com>
2022-09-25 14:30:32 -0300

    heroku

--- packages/server/ormconfig.js
+++ packages/server/ormconfig.js
@@ -1,15 +1,18 @@
 const isDevelopment = !!(process.env.NODE_ENV === 'development')
 
+const folderProd = isDevelopment ? 'src' : 'dist'
+const filesProd = isDevelopment ? '.ts' : '.js'
+
 const environments = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: [`${isDevelopment ? 'src' : 'dist'}/modules/**/entities/*{.ts,.js}`],
-  migrations: [`${isDevelopment ? 'src' : 'dist'}/shared/infra/typeorm/migrations/*{.ts,.js}`],
-  seeds: [`${isDevelopment ? 'src' : 'dist'}/shared/infra/typeorm/seeds/**/*{.ts,.js}`],
-  factories: [`${isDevelopment ? 'src' : 'dist'}/shared/infra/typeorm/factories/**/*{.ts,.js}`],
+  entities: [`${folderProd}/modules/**/entities/*${filesProd}`],
+  migrations: [`${folderProd}/shared/infra/typeorm/migrations/*${filesProd}`],
+  seeds: [`${folderProd}/shared/infra/typeorm/seeds/**/*${filesProd}`],
+  factories: [`${folderProd}/shared/infra/typeorm/factories/**/*${filesProd}`],
   cli: {
-    entitiesDir: 'src/modules/**/entities',
-    migrationsDir: 'src/shared/infra/typeorm/migrations'
+    entitiesDir: `${folderProd}/modules/**/entities`,
+    migrationsDir: `${folderProd}/shared/infra/typeorm/migrations`,
   }
 }
 
