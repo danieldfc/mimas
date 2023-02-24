7c1251b2abcdd318995f0ff7880d78f57e570238
danielfelizardo <daniel.david772@gmail.com>
2023-02-23 22:53:34 -0300

    Cria site público inicial configuracao

---
+++ packages/client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    backgroundImage: {
+      app: 'linear-gradient(90deg, #2a0d41, #5e00a3)'
+    }
+  },
+  plugins: [
+  ],
+}
